0fcb7ba801d393fe29e04e5bfdb94d8a5f989332
fmauff1978 <fmauff1978@gmail.com>
2022-03-31 10:49:48 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductService } from './product.service';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductsComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductsComponent, RatingComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
